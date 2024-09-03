66fbcae50c118ede7d6ebb817ddf8477ae38bc73
Prashant <prashantco111@gmail.com>
2024-09-03 15:54:49 +0530

    Added singup route

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
 This is a test project to learn and practice Go concepts by building projects. In this project I am building a small URL Shortner.
 
 ### Features:
-- [ ] Authentication with email and password.
+- [ ] Authentication with username and password.
 - [ ] APIKey validation for Routes.
 - [ ] User passes a URL, it should return a small URL for that user.
 - [ ] Add base analytics of URL used.
 
 ### Steps:
-- [ ] Use Go-Chi for Server framework.
+- [x] Use Go-Chi for Server framework.
 - [x] Initialize Mongo DB.
 - [ ] Implement User Authentication.
 - [ ] Implement APIKey validation and usage.
--- user/user.go
+++ user/user.go
@@ -1,18 +1,34 @@
 package user
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/go-chi/chi"
+	"github.com/prashant1k99/URL-Shortner/db"
 	"github.com/prashant1k99/URL-Shortner/utils"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type UserResources struct{}
 
 type user struct {
-	ID       int    `json:"id"`
-	Username string `json:"username"`
-	APIKey   string `json:"apiKey"`
+	ID       primitive.ObjectID `json:"id"`
+	Username string             `json:"username"`
+}
+
+type userWithPassword struct {
+	user
+	Password string `json:"password"`
+}
+
+type userWithAPI struct {
+	user
+	APIKey string `json:"apiKey"`
 }
 
 func (rs UserResources) Routes() chi.Router {
@@ -26,9 +42,43 @@ func (rs UserResources) Routes() chi.Router {
 }
 
 func (rs UserResources) signUp(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	params := userWithPassword{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	if params.Username == "" || params.Password == "" {
+		utils.RespondWithError(w, http.StatusBadRequest, "Username and password are required to Signup")
+		return
+	}
+	userCollection, err := db.GetCollection("users")
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+	encryptedPass, err := utils.EncryptPassword(params.Password)
+	if err != nil {
+		utils.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
+		return
+	}
+	result, err := userCollection.InsertOne(context.TODO(), bson.M{
+		"username": params.Username,
+		"password": encryptedPass,
+	})
+	if err != nil {
+		message := err.Error()
+		if strings.Contains(err.Error(), "duplicate key error collection:") {
+			message = "User with this username already exists"
+		}
+		utils.RespondWithError(w, http.StatusInternalServerError, message)
+		return
+	}
+	fmt.Printf("Inserted a document: %v\n", result.InsertedID)
 	utils.RespondWithJSON(w, http.StatusCreated, user{
-		ID:       1,
-		Username: "Prashant",
-		APIKey:   "some API",
+		ID:       result.InsertedID.(primitive.ObjectID),
+		Username: params.Username,
 	})
 }
